9088e923d56b9b994c1a7caa0584bd14b4438c50
Mikhail Butorin <mikhail.butorin@axenix.pro>
2024-02-19 17:11:08 +0600

    added task 19/02/2024

--- StudentService/src/main/java/org/example/Main.java
+++ StudentService/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Дата в строковом формате
+        String dateString = "29-04-1993";
+
+        // Форматтер для преобразования строки в LocalDate
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        // Преобразование строки в LocalDate
+        LocalDate birthDate = LocalDate.parse(dateString, formatter);
+
+        // Текущая дата
+        LocalDate currentDate = LocalDate.now();
+
+        // Расчет периода между датами
+        Period period = Period.between(birthDate, currentDate);
+
+        // Вывод количества лет
+        System.out.println("Возраст: " + period.getYears() + " лет");
     }
 }
