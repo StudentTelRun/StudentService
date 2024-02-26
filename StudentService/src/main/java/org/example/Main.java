30a8c8a23485e36d9271bb8827d94a2a780a4a7f
Mikhail Butorin <mikhail.butorin@axenix.pro>
2024-02-26 21:41:44 +0600

    added sqlite database, added function for taking connection for SQLite

--- StudentService/src/main/java/org/example/Main.java
+++ StudentService/src/main/java/org/example/Main.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import org.example.component.DatabaseConnection;
 import org.example.data.Student;
 import org.example.service.Uni;
 import org.example.service.cashService.StudentsMap;
+import org.example.service.dmlService.impl.PostgreDML;
+
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.INFO;
 
 public class Main {
-    public static void main(String[] args) {
+
+    private static Logger logger = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws SQLException {
 //        // Дата в строковом формате
 //        String dateString = "29-04-1993";
 //e
@@ -24,12 +34,25 @@ public class Main {
 //        // Вывод количества лет
 //        System.out.println("Возраст: " + period.getYears() + " лет");
 
-        StudentsMap studentsMap = StudentsMap.GET_INSTANCE();
+    /*    StudentsMap studentsMap = StudentsMap.GET_INSTANCE();
         studentsMap.addStudent(new Student("Alex", "A", 20));
         studentsMap.addStudent(new Student("Ben", "B", 23));
         studentsMap.addStudent(new Student("Clark", "C", 25));
 
         Uni uni = new Uni();
-        uni.study();
+        uni.study();*/
+
+        //пример того как не правильно генерит AI
+        try (var sqlIiteConnection = DatabaseConnection.INSTANCE.getSQLiteConnection();
+             var statement = sqlIiteConnection.createStatement()) {
+            var result = statement.executeQuery("SELECT * FROM student");
+            while (result.next()) {
+                logger.info("Name from select %s".formatted(result.getString("name")));
+            }
+        }
+        //все работает задание запустить код.
+        //и проверить что работаее
+
+
     }
 }
---
+++ StudentService/src/main/java/org/example/component/DatabaseConnection.java
@@ -0,0 +1,56 @@
+package org.example.component;
+
+import org.postgresql.PGProperty;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+//TODO добавить абстрацию
+public enum DatabaseConnection {
+    INSTANCE;
+
+    private static Connection getSQLiteInstance() throws SQLException {
+        //url тут просто ссылка на файл а не адрес в сети, аутеникфикация не поддерживается этой базой
+        String url = "jdbc:sqlite:src/main/resources/db/student.db";
+        var con = DriverManager.getConnection(url);
+
+        return con;
+    }
+
+    private static Connection getPGInstance() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/postgres";
+        Properties props = new Properties();
+        PGProperty.USER.set(props, "postgres");
+        PGProperty.PASSWORD.set(props, "admin");
+        PGProperty.ASSUME_MIN_SERVER_VERSION.set(props, "10");
+        PGProperty.REPLICATION.set(props, "database");
+        PGProperty.PREFER_QUERY_MODE.set(props, "simple");
+        var con = DriverManager.getConnection(url, props);
+
+        return con;
+    }
+
+    //TODO переделать на дженеричный вариант
+    public Connection getPGConnection() throws SQLException {
+        try {
+            return this.getPGInstance();
+        } catch (SQLException e) {
+            throw  new RuntimeException(e);
+        }
+    }
+
+
+    //тут тоже самое что и в предыдущем методе только порлучаем базу лайт
+    public Connection getSQLiteConnection() throws SQLException {
+        try {
+            return this.getSQLiteInstance();
+        } catch (SQLException e) {
+            throw  new RuntimeException(e);
+        }
+    }
+
+
+}
